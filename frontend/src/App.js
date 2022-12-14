0351ea4f8faf9a384eecd8d4711611bdc7cc27ec
pratikderepatil <pratikdere333@gmail.com>
2022-12-14 18:20:00 +0530

    Signup Front end complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
+import { Button, useDisclosure } from "@chakra-ui/react";
+import Signup from "./components/Signup";
 
 function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	return (
+		<div className="App">
+			<Button onClick={onOpen}>Open Modal</Button>
+			<Signup isOpen={isOpen} onClose={onClose} />
+		</div>
+	);
 }
 
 export default App;

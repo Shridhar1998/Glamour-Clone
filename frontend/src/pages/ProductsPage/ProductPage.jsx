f0d4723b60c48b6966cc3d5ef4fe7d4688f7d672
unknown <shridharhakke1998@gmail.com>
2022-12-18 01:15:11 +0530

    sorting added

--- frontend/src/pages/ProductsPage/ProductPage.jsx
+++ frontend/src/pages/ProductsPage/ProductPage.jsx
@@ -31,8 +31,10 @@ function ProductPage() {
   const [prod, setProd] = useState([]);
   const { category } = useParams();
   const [sortBy, setSortBy] = useState("asc");
+  const [page,setPage]=useState(2)
   function handleGet() {
-    axios(`https://glamour.onrender.com/products/${category}?page=${3}`).then((res) => {
+    
+    axios(`https://glamour.onrender.com/products/${category}?page=${page}`).then((res) => {
       setProd(res.data);
       // console.log(res, "prod");
       localStorage.setItem("data", JSON.stringify(res.data));
@@ -59,6 +61,9 @@ function ProductPage() {
   console.log(prod);
   useEffect(() => {
     setProd(dummydata);
+    if(category=="gel"){
+setPage(1)
+    }
   }, []);
 
   useEffect(() => {
